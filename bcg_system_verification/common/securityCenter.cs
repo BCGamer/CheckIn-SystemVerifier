4560a6aa6fea61eb1b79d10f719741b61cb763b7
Jason <Jason@wallacetech.com>
2015-02-12 22:29:29 -0800

    Removed Commented out code

--- bcg_system_verification/common/securityCenter.cs
+++ bcg_system_verification/common/securityCenter.cs
@@ -48,48 +48,6 @@ namespace bcg_system_verification.common
             return results;
         }
 
-        /*
-        private static string windows6x(ManagementObject mo)
-        {
-            //Vista, 7, 8, 8.1
-            //Have to convert the object -> String -> Int -> Hex (String)
-            //Once we have the Hex we can see the status based on the last 4 digits
-            string productStateStr = (mo["ProductState"].ToString());
-            int productStateInt = Convert.ToInt32(productStateStr);
-            string productStateHex = productStateInt.ToString("X");
-
-            if (Globals.debugMode) Console.WriteLine("ProductStateInt: " + productStateInt);
-            if (Globals.debugMode) Console.WriteLine("ProductStateHex: " + productStateHex);
-
-            string enabledStatus = "null";
-            string updateStatus = "null";
-
-            if (productStateHex.Substring(1, 2) == "10") { enabledStatus = "enabled"; };
-            if (productStateHex.Substring(1, 2) == "11") { enabledStatus = "enabled"; };
-            if (productStateHex.Substring(1, 2) == "00") { enabledStatus = "disabled"; };
-            if (productStateHex.Substring(1, 2) == "01") { enabledStatus = "disabled"; };
-            if (Globals.debugMode) Console.WriteLine("enabledStatus: " + enabledStatus);
-
-            if (productStateHex.Substring(3, 2) == "00") { updateStatus = "up_to_date"; };
-            if (productStateHex.Substring(3, 2) == "10") { updateStatus = "not_updated"; };
-            if (Globals.debugMode) Console.WriteLine("updateStatus: " + updateStatus);
-            
-            if (Globals.debugMode) Console.WriteLine("");
-
-            if (enabledStatus != "null" && updateStatus != "null")
-            {
-                if (Globals.debugMode) Console.WriteLine("Returning status details from securityCenter.windows6x()");
-                return enabledStatus + "," + updateStatus;
-            }
-            else
-            {
-                if (Globals.debugMode) Console.WriteLine("Returning PROBLEM status from securityCenter.windows6x()");
-                return "problem";
-            }
-
-            
-        }*/
-
         private static string windows5x(ManagementObject mo)
         {
             if (Globals.debugMode) Console.WriteLine("**NEEDS TESTING**");
